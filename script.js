225e3763f9de086d3324c0cbbc683a8e45cc6311
KunOfficialLb <145544378+KunOfficialLb@users.noreply.github.com>
2023-09-27 22:18:05 +0300

    Update script.js
    Signed-off-by: KunOfficialLb <145544378+KunOfficialLb@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-// JavaScript code for interactive background
+// JavaScript code for interactive neon background with ripple effect
+const neonContainer = document.querySelector('.neon-container');
 const neonBackground = document.querySelector('.neon-background');
 
-document.addEventListener('mousemove', (e) => {
-    const mouseX = e.clientX / window.innerWidth;
-    const mouseY = e.clientY / window.innerHeight;
+// Function to create a ripple element
+function createRipple(event) {
+    const ripple = document.createElement('div');
+    ripple.classList.add('ripple');
+    neonContainer.appendChild(ripple);
+
+    const rect = neonContainer.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+
+    ripple.style.width = ripple.style.height = `${size}px`;
+
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    ripple.style.left = `${mouseX - size / 2}px`;
+    ripple.style.top = `${mouseY - size / 2}px`;
+
+    ripple.addEventListener('animationend', () => {
+        ripple.remove();
+    });
+}
+
+neonContainer.addEventListener('click', (event) => {
+    createRipple(event);
+    const mouseX = event.clientX / window.innerWidth;
+    const mouseY = event.clientY / window.innerHeight;
 
     // Adjust the background position based on mouse cursor
-    neonBackground.style.backgroundPosition = `${mouseX * 200}% ${mouseY * 200}%`;
+    const backgroundX = mouseX * 100;
+    const backgroundY = mouseY * 100;
+    neonBackground.style.backgroundPosition = `${backgroundX}% ${backgroundY}%`;
 });
