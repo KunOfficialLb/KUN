7355e6694b102bfe3c3c4b732d9e783f8f95ff0b
KunOfficialLb <145544378+KunOfficialLb@users.noreply.github.com>
2023-09-29 10:19:38 +0300

    Update script.js
    Signed-off-by: KunOfficialLb <145544378+KunOfficialLb@users.noreply.github.com>

--- script.js
+++ script.js
@@ -56,20 +56,4 @@ function createRipple(event) {
     const mouseY = event.clientY - rect.top;
 
     ripple.style.left = `${mouseX - size / 2}px`;
-    ripple.style.top = `${mouseY - size / 2}px`;
-
-    ripple.addEventListener('animationend', () => {
-        ripple.remove();
-    });
-}
-
-neonContainer.addEventListener('click', (event) => {
-    createRipple(event);
-    const mouseX = event.clientX / window.innerWidth;
-    const mouseY = event.clientY / window.innerHeight;
-
-    // Adjust the background position based on mouse cursor
-    const backgroundX = mouseX * 100;
-    const backgroundY = mouseY * 100;
-    neonBackground.style.backgroundPosition = `${backgroundX}% ${backgroundY}%`;
-});
+    ripple.style.top = `${mouseY - size / 2
