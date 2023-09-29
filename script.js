dab1c2d9a9c59ca1189b6809d537ebd88f376411
KunOfficialLb <145544378+KunOfficialLb@users.noreply.github.com>
2023-09-29 10:11:35 +0300

    Update script.js
    Signed-off-by: KunOfficialLb <145544378+KunOfficialLb@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 // JavaScript code for interactive neon background with ripple effect
 const neonContainer = document.querySelector('.neon-container');
 const neonBackground = document.querySelector('.neon-background');
+const carousels = document.querySelectorAll('.instagram-carousel');
+
+carousels.forEach((carousel) => {
+    const container = carousel.querySelector('.carousel-container');
+    const slides = carousel.querySelectorAll('.carousel-slide');
+
+    let currentIndex = 0;
+
+    function updateCarousel() {
+        const slideWidth = slides[0].offsetWidth;
+        container.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        updateCarousel();
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        updateCarousel();
+    }
+
+    const nextButton = document.createElement('button');
+    nextButton.textContent = 'Next';
+    nextButton.addEventListener('click', nextSlide);
+
+    const prevButton = document.createElement('button');
+    prevButton.textContent = 'Prev';
+    prevButton.addEventListener('click', prevSlide);
+
+    const nav = document.createElement('div');
+    nav.classList.add('carousel-nav');
+    nav.appendChild(prevButton);
+    nav.appendChild(nextButton);
+
+    carousel.appendChild(nav);
+    updateCarousel();
+});
 
 // Function to create a ripple element
 function createRipple(event) {
