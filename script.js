617d127627bd0f5fe3f1f0fde5cdacd78c6b67d2
KunOfficialLb <145544378+KunOfficialLb@users.noreply.github.com>
2023-09-29 11:17:01 +0300

    Update script.js
    Signed-off-by: KunOfficialLb <145544378+KunOfficialLb@users.noreply.github.com>

--- script.js
+++ script.js
@@ -19,24 +19,16 @@ carousels.forEach((carousel) => {
         updateCarousel();
     }
 
-    function prevSlide() {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        updateCarousel();
+    function startAutoSlide() {
+        setInterval(nextSlide, 10000); // Switch slides every 10 seconds
     }
 
-    const nextButton = document.createElement('button');
-    nextButton.textContent = 'Next';
-    nextButton.addEventListener('click', nextSlide);
-
-    const prevButton = document.createElement('button');
-    prevButton.textContent = 'Prev';
-    prevButton.addEventListener('click', prevSlide);
+    // Start auto-slide when the page loads
+    startAutoSlide();
 
     const nav = document.createElement('div');
     nav.classList.add('carousel-nav');
-    nav.appendChild(prevButton);
-    nav.appendChild(nextButton);
-
+    
     carousel.appendChild(nav);
     updateCarousel();
 });
@@ -56,4 +48,147 @@ function createRipple(event) {
     const mouseY = event.clientY - rect.top;
 
     ripple.style.left = `${mouseX - size / 2}px`;
-    ripple.style.top = `${mouseY - size / 2
+    ripple.style.top = `${mouseY - size / 2}px`;
+
+    ripple.addEventListener('animationend', () => {
+        ripple.remove();
+    });
+}
+
+neonContainer.addEventListener('click', (event) => {
+    createRipple(event);
+    const mouseX = event.clientX / window.innerWidth;
+    const mouseY = event.clientY / window.innerHeight;
+
+    // Adjust the background position based on mouse cursor
+    const backgroundX = mouseX * 5; // Adjust the sensitivity as needed
+    const backgroundY = mouseY * 5; // Adjust the sensitivity as needed
+    neonBackground.style.backgroundPosition = `${backgroundX}% ${backgroundY}%`;
+});
+
+/* Reset some default styles for better consistency */
+body, h1, h2, p, ul, li {
+    margin: 0;
+    padding: 0;
+}
+
+/* Basic styling */
+body {
+    font-family: Arial, sans-serif;
+    background: transparent;
+    color: #000;
+    overflow: hidden;
+    margin: 0;
+}
+
+.neon-container {
+    position: relative;
+}
+
+header {
+    background-color: black;
+    color: #9900cc;
+    text-align: center;
+    padding: 20px;
+}
+
+nav ul {
+    list-style-type: none;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 20px;
+}
+
+nav ul li a {
+    text-decoration: none;
+    color: #9900cc;
+}
+
+.logo {
+    text-align: center;
+}
+
+.logo img {
+    width: 100px; /* Adjust the size as needed */
+}
+
+main {
+    max-width: 800px;
+    margin: 40px auto;
+    padding: 20px;
+    background: transparent;
+    color: #000;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+}
+
+footer {
+    text-align: center;
+    padding: 20px;
+    background: transparent;
+    color: #000;
+}
+
+/* Neon Background with Color Palette */
+.neon-background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    pointer-events: none;
+    background: radial-gradient(circle, #6e0080, #9900cc, #000066, #9966cc);
+    background-size: 200% 200%;
+    animation: gradient 5s linear infinite;
+}
+
+/* Add this CSS to style the carousel */
+.instagram-carousel {
+    width: 100%;
+    overflow: hidden;
+    position: relative;
+}
+
+.carousel-container {
+    display: flex;
+    overflow: hidden;
+}
+
+.carousel-slide {
+    flex: 0 0 auto;
+    width: 100%;
+    transition: transform 0.5s ease;
+}
+
+.carousel-slide img {
+    width: 100%;
+    height: auto;
+    display: block;
+}
+
+/* Style arrows for navigation (optional) */
+.carousel-nav {
+    text-align: center;
+    margin-top: 10px;
+}
+
+.carousel-nav button {
+    background: #333;
+    color: #fff;
+    border: none;
+    padding: 5px 10px;
+    margin: 0 5px;
+    cursor: pointer;
+}
+
+@keyframes gradient {
+    0% {
+        background-position: 0% 0%;
+    }
+    100% {
+        background-position: 100% 100%;
+    }
+}
